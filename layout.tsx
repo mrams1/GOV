c6832257b62f6cc5e54c1ebd66c30501c816d5d9
mrams1 <137014047+mrams1@users.noreply.github.com>
2025-12-07 06:08:18 +0700

    Add files via upload

--- layout.tsx
+++ layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
+import "./globals.css";
+import Navigation from "./components/navigation";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,12 +17,12 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   icons:
     "https://cdn.discordapp.com/attachments/1382019709989290004/1382020202946101449/IMG_8208.png?ex=6892249d&is=6890d31d&hm=3ef25c941e0e30f1b1ed7c56432f7c6e84b346da3f59b135ce38b135951376b9&",
-  title: "Maintenance Mode | MLRPCrafting - MotionLife RP Calculator",
+  title: "IME RP Crafting Optimizer | MINING",
   description:
-    "Website sedang dalam perbaikan. Calculator crafting MotionLife RP akan segera kembali dengan fitur yang lebih baik.",
+    "Optimize your crafting and mining in IME RP with our powerful tool. Calculate profits, time, and requirements for all items.",
 };
 
-export default function MaintenanceLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -30,6 +32,7 @@ export default function MaintenanceLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SpeedInsights />
         {children}
       </body>
     </html>
